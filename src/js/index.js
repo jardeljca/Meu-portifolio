8db450924ea8d329a37c4dc9d0af691a9fff065c
Jardel Queiroz <jardeljcadm@gmail.com>
2024-06-05 06:56:33 -0300

    Header com js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,24 @@
-window.addEventListener('load', function(){
-    document.getElementById('h1Header').classList.add('loader');
+window.addEventListener("load", function () {
+  document.getElementById("h1Header").classList.add("loader");
 });
+
+const botaoMenu = document.getElementById("iconeMenu");
+const botaoMenuX = document.getElementById("iconeMenuX");
+const menuNav = document.getElementById("menuNav");
+const header = document.getElementById("header");
+var larguraDaJanela = window.innerWidth;
+
+menuNav.style.display = "none";
+
+if (larguraDaJanela < 560) {
+  
+    botaoMenu.addEventListener("click", () => {
+      header.style.display = "none";
+      menuNav.style.display = "block";
+    });
+
+    botaoMenuX.addEventListener("click", () => {
+      menuNav.style.display = "none";
+      header.style.display = "block";
+    });
+}
